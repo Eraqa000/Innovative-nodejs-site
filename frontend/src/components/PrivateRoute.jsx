6799d8267d046bbb3f11ebfecb269ec4a112462e
Eraqa000 <154332563+Eraqa000@users.noreply.github.com>
2025-06-14 04:18:23 +0500

    Update PrivateRoute.jsx

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -6,12 +6,14 @@ export default function PrivateRoute({ children }) {
   const [isAuth, setIsAuth] = useState(false);
 
   useEffect(() => {
-    fetch("http://localhost:5000/api/auth/me", {
-      credentials: "include",
-    })
-      .then((res) => setIsAuth(res.ok))
-      .finally(() => setLoading(false));
-  }, []);
+  fetch(`${import.meta.env.VITE_API_URL}/api/auth/me`, {
+    credentials: "include",
+  })
+    .then((res) => setIsAuth(res.ok))
+    .catch(() => setIsAuth(false))
+    .finally(() => setLoading(false));
+}, []);
+
 
   if (loading) return null; // или спиннер
   return isAuth ? children : <Navigate to="/login" />;
